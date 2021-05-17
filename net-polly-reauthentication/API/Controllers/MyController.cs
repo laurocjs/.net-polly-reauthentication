773b85d80d267c49c7cdb7c6e3dbdffe667ec33b
Lauro Jacques <lauro.jacques@dtidigital.com.br>
2021-05-17 16:51:00 -0300

    Some general changes, including a controller

---
+++ net-polly-reauthentication/API/Controllers/MyController.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyController : ControllerBase
+    {
+        private readonly ISomeService _someService;
+
+        public MyController(ISomeService someService)
+        {
+            _someService = someService;
+        }
+
+        /// <summary>
+        /// Just any get, list your available resources
+        /// </summary>
+        /// <returns>A list with your resources</returns>
+        [HttpGet]
+        [Route("")]
+        [ProducesResponseType(typeof(List<string>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> List()
+        {
+            var result = await _someService.GetSomething();
+
+            if (result != null && result.Any())
+                return Ok(result);
+
+            return NotFound("Couldn't retrieve the requested data");
+        }
+    }
+}
